aad99dbc20cb563cf1bf203853b68a3df1ae9874
luseco <ibraheem04a@gmail.com>
2023-11-13 09:54:58 +0100

    yupdate

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-﻿//                                                                   |
-//Square of info
+﻿/*                                                                   |
+To Do:
+Square of info
+fill randomly with input %
+make symbols customizable
+check if row/column matches description
+bug with max desc size where last doesnt get printed
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -51,6 +57,35 @@ bool* getCol(int pos, bool dsp[][50]) {
 	return col;
 }
 
+int intRequest(string consoleText, int max) {
+	string userInput;
+	int number = 0;
+
+	while (true) {
+		number = 0;
+		cout << consoleText;
+		cin >> userInput;
+
+		for (int i = 0; i < userInput.size(); i++) {
+			if (userInput[i] >= '0' && userInput[i] <= '9') {
+				number = append(number, CharToInt(userInput[i]));
+			}
+		}
+		if (number > max) {
+			clear(1, 0);
+			cout << "Maximum input is " << max << "." << endl;
+		}
+		else if (number == 0) {
+			clear(1, 1);
+		}
+		else {
+			break;
+		}
+	}
+	clear(1, 0);
+	return number;
+}
+
 class Nonogram {
 private:
 	int rowI = 0, colI = 0;
@@ -58,72 +93,13 @@ private:
 	string line;
 
 public:
-	ifstream inputFile;
 	const int max = 50;
-	int dimensions[2] = {0};
+	int dimensions[2] = {15, 15};
 	int rows[50][25] = {0}, cols[50][25] = {0};
 	bool display[50][50] = {0};
 	int cursor[2] = {0};
 	string w = {char(176), char(176)}, b = {char(178), char(178)},
 		   wC = "  ", bC = {char(219), char(219)};
-
-	Nonogram() {
-		dimensions[0] = 13;
-		dimensions[1] = 13;
-	}
-
-	Nonogram(string fileName) : inputFile(fileName) {
-		getline(inputFile, line);
-		for (int i = 0; i < line.size(); i++) {
-			if (line[i] == ' ') {
-				dimension++;
-			}
-			else {
-				dimensions[dimension] = append(dimensions[dimension],
-										CharToInt(line[i]));
-			}
-		}
-
-		for (int i = 0; i < dimensions[0]; i++) {
-			getline(inputFile, line);
-			for (int ii = 0; ; ii++) {
-				if (line[ii] == ' ') {
-					rowI++;
-				}
-				else if (line[ii] == '0') {
-					rowI = 0;
-					break;
-				}
-				else {
-					rows[i][rowI] = append(rows[i][rowI],
-									CharToInt(line[ii]));
-				}
-			}
-		}
-
-		for (int i = 0; i < dimensions[1]; i++) {
-			getline(inputFile, line);
-			for (int ii = 0; ; ii++) {
-				if (line[ii] == ' ') {
-					colI++;
-				}
-				else if (line[ii] == '0') {
-					colI = 0;
-					break;
-				}
-				else {
-					cols[i][colI] = append(cols[i][colI],
-									CharToInt(line[ii]));
-				}
-			}
-		}
-		inputFile.close();
-
-		int temp;
-		temp = dimensions[0];
-		dimensions[0] = dimensions[1];
-		dimensions[1] = temp;
-	}
 
 	void toggle(int x, int y) {
 		display[x][y] = !display[x][y];
@@ -162,11 +138,18 @@ public:
 		
 		cout << char(200) << string(dimensions[0] * 2.0, char(205))
 			 << char(188) << endl;
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 25; i++) {
 			int counter = 0;
+			cout << ' ';
+
 			for (int ii = 0; ii < dimensions[0]; ii++) {
 				if (cols[ii][i] != 0) {
-					cout << ' ' << cols[ii][i];
+					if (cols[ii][i] >= 10) {
+						cout << cols[ii][i];
+					}
+					else {
+						cout << ' ' << cols[ii][i];
+					}
 				}
 				else {
 					cout << "  ";
@@ -196,32 +179,12 @@ public:
 		}
 	}
 
-	int intRequest(string consoleText) {
-		string userInput;
-		int number = 0;
-
-		while (true) {
-			number = 0;
-			cout << consoleText;
-			cin >> userInput;
-
-			for (int i = 0; i < userInput.size(); i++) {
-				if (userInput[i] >= '0' && userInput[i] <= '9') {
-					number = append(number, CharToInt(userInput[i]));
-				}
-			}
-			if (number > max) {
-				clear(1, 0);
-				cout << "Maximum input is " << max << "." << endl;
-			} else if (number == 0) {
-				clear(1, 1);
-			}
-			else {
-				break;
+	void clean() {
+		for (int i = 0; i < sizeof(display[0]) / 4; i++) {
+			for (int ii = 0; ii < sizeof(display[0]) / 4; ii++) {
+				display[i][ii] = 0;
 			}
 		}
-		clear(1, 0);
-		return number;
 	}
 
 	int* dsc(bool dsp[]) {
@@ -229,7 +192,7 @@ public:
 		int counter = 0;
 
 		for (int i = 0, ii = 0; i < max; i++) {
-			if (dsp[i] == 0 && counter != 0) {
+			if ((dsp[i] == 0 && counter != 0) || i == max - 1) {
 				description[ii] = counter;
 				counter = 0;
 				ii++;
@@ -242,12 +205,75 @@ public:
 	}
 
 	void dscDsp() {
-		for (int i = 0; i < 50; i++) {
-			for (int ii = 0; ii < 25; ii++) {
+		bool list[25];
+
+		for (int i = 0; i < max; i++) {
+			for (int ii = 0; ii < max / 2; ii++) {
+				list[ii] = getCol(i, display)[ii];
+			}
+
+			for (int ii = 0; ii < max / 2; ii++) {
 				rows[i][ii] = dsc(display[i])[ii];
-				cols[i][ii] = dsc(getCol(i, display))[ii];
+				cols[i][ii] = dsc(list)[ii];
+			}
+			cout << endl;
+		}
+	}
+
+	void import(ifstream& inputFile) {
+		getline(inputFile, line);
+		dimensions[0] = 0;
+		dimensions[1] = 0;
+
+		for (int i = 0; i < line.size(); i++) {
+			if (line[i] == ' ') {
+				dimension++;
+			}
+			else {
+				dimensions[dimension] = append(dimensions[dimension],
+					CharToInt(line[i]));
 			}
 		}
+
+		for (int i = 0; i < dimensions[0]; i++) {
+			getline(inputFile, line);
+			for (int ii = 0; ; ii++) {
+				if (line[ii] == ' ') {
+					rowI++;
+				}
+				else if (line[ii] == '0') {
+					rowI = 0;
+					break;
+				}
+				else {
+					rows[i][rowI] = append(rows[i][rowI],
+						CharToInt(line[ii]));
+				}
+			}
+		}
+
+		for (int i = 0; i < dimensions[1]; i++) {
+			getline(inputFile, line);
+			for (int ii = 0; ; ii++) {
+				if (line[ii] == ' ') {
+					colI++;
+				}
+				else if (line[ii] == '0') {
+					colI = 0;
+					break;
+				}
+				else {
+					cols[i][colI] = append(cols[i][colI],
+						CharToInt(line[ii]));
+				}
+			}
+		}
+		inputFile.close();
+
+		int temp;
+		temp = dimensions[0];
+		dimensions[0] = dimensions[1];
+		dimensions[1] = temp;
 	}
 };
 
@@ -257,26 +283,33 @@ void Properties(Nonogram& nono) {
 	clear(1, 0);
 	bool x = true;
 	char input[2];
+	string fileName;
+	fstream inputFile;
 
 	while (x) {
 		nono.show();
 		cout << endl << "(d)imensions | (c)lean | (f)ill randomly "
-			 << "| (m)ake descriptions | s(y)mbols | (b)ack" << endl;
+			 << "| (m)ake descriptions" << endl
+			 << "(s)ymbols | (i)mport | (b)ack" << endl;
 		cin.get(input, 2);
 
 		switch (tryUncap(input[0])) {
 		case 'd':
 			clear(1, 0);
-			nono.dimensions[0] = nono.intRequest("x: ");
-			nono.dimensions[1] = nono.intRequest("y: ");
+
+			nono.dimensions[0] = intRequest("x: ", nono.max);
+			nono.dimensions[1] = intRequest("y: ", nono.max);
+
+			nono.cursor[0] = 0;
+			nono.cursor[1] = 0;
+
+			nono.clean();
+			nono.dscDsp();
+			
+			system("CLS");
 			break;
 		case 'c':
-			for (int i = 0; i < sizeof(nono.display[0]) / 4; i++) {
-				for (int ii = 0; ii < sizeof(nono.display[0]) 
-					/ 4; ii++) {
-					nono.display[i][ii] = { 0 };
-				}
-			}
+			nono.clean();
 			clear(1, 0);
 			break;
 		case 'f':
@@ -285,7 +318,32 @@ void Properties(Nonogram& nono) {
 			nono.dscDsp();
 			clear(1, 0);
 			break;
-		case 'y':
+		case 's':
+			break;
+		case 'i':
+			system("CLS");
+		{
+			while (true) {
+				cout << "File name: ";
+				cin >> fileName;
+				ifstream inputFile(fileName);
+				
+				if (inputFile.good()) {
+					nono.import(inputFile);
+					break;
+				}
+				else if (fileName == "STOP") {
+					break;
+				}
+				else {
+					inputFile.close();
+					clear(1, 0);
+					cout << "File doesn't exist. Try Again. "
+						 << "To exit type STOP." << endl;
+				}
+			}
+		}
+			clear(1, 0);
 			break;
 		case 'b':
 			clear(1, 0);
@@ -298,7 +356,7 @@ void Properties(Nonogram& nono) {
 }
 
 void MainMenu() {
-	Nonogram nono("pi.txt");
+	Nonogram nono;
 	char input[2];
 		
 	while(true) {
